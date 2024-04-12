e10b3143c2248c49baf648cf2892b8ca8112d20c
macisnotcoding <codingmac12@gmail.com>
2024-04-12 18:02:47 +0200

    feat: add solved exercise for RetosMoureDev number 11

--- RetosMoureDev/Ejercicios.cs
+++ RetosMoureDev/Ejercicios.cs
@@ -587,5 +587,69 @@ namespace RetosMoureDev
         }
 
         #endregion
+
+        #region Ejercicio 11
+        /// <summary>
+        /// Crea un programa que comprueba si los paréntesis, llaves y corchetes
+        /// de una expresión están equilibrados.
+        /// - Equilibrado significa que estos delimitadores se abren y cierran
+        ///   en orden y de forma correcta.
+        /// - Paréntesis, llaves y corchetes son igual de prioritarios.
+        ///   No hay uno más importante que otro.
+        /// - Expresión balanceada: { [ a * ( c + d ) ] - 5 }
+        /// - Expresión no balanceada: { a * ( c + d ) ] - 5 }
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// Dificultad: Medio
+        /// </remarks>
+        public static void N11(string expresion)
+        {
+            if(EsExpresionBalanceada(expresion))
+            {
+                Console.WriteLine($"La expresion \'{expresion}' esta balanceada");
+            }
+            else
+            {
+                Console.WriteLine($"La expresion \'{expresion}' NO esta balanceada");
+            }
+        }
+
+        private static bool EsExpresionBalanceada(string expresion)
+        {
+            Dictionary<char, char> simbolos = new Dictionary<char, char>
+            {
+                {'{', '}'},
+                {'[', ']'},
+                {'(', ')'}
+            };
+
+            //Usamos un stack para guardar los delimitadores que se abren
+            //Cuando encontramos uno que se cierra, lo sacamos del stack
+            //Si al final del recorrido del string, el stack esta vacio, la expresion esta balanceada
+            //Si no, no lo esta
+            Stack<char> delimitadoresAbiertos = new Stack<char>();
+
+            foreach(char caracter in expresion)
+            {
+                //Si el caracter es un delimitador que se abre, lo metemos en el stack
+                if(simbolos.ContainsKey(caracter))
+                {
+                    delimitadoresAbiertos.Push(caracter);
+                }//Si el caracter es un delimitador que se cierra, comprobamos que el ultimo delimitador que se abrio sea el que se cierra
+                else if(simbolos.ContainsValue(caracter))
+                {
+                    //Si no hay delimitadores abiertos o el ultimo delimitador abierto no es el que se cierra, la expresion no esta balanceada
+                    if(delimitadoresAbiertos.Count == 0 || caracter != simbolos[delimitadoresAbiertos.Pop()])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            //Si al final del recorrido del string, el stack esta vacio, la expresion esta balanceada
+            return delimitadoresAbiertos.Count == 0;
+        }
+        #endregion
     }
 }
