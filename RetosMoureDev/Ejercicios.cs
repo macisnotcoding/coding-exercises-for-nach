af4d8009800b50bb8061f2239b8832cae149cd77
macisnotcoding <codingmac12@gmail.com>
2024-04-12 02:00:52 +0200

    feat: add solved excercices from RetosMoureDev up to number 10

--- RetosMoureDev/Ejercicios.cs
+++ RetosMoureDev/Ejercicios.cs
@@ -1,4 +1,6 @@
 ﻿using RetosMoureDev.Models.Poligonos;
+using System.Drawing;
+using System.Text;
 
 namespace RetosMoureDev
 {
@@ -12,11 +14,11 @@ namespace RetosMoureDev
         /// - Múltiplos de 3 por la palabra "fizz".
         /// - Múltiplos de 5 por la palabra "buzz".
         /// - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+        /// </summary>
         /// 
         /// <remarks>
         /// Dificultad: Facil
         /// </remarks>
-        /// </summary>
         public static void N1()
         {
             for (int i = 1; i <= 100; i++)
@@ -51,11 +53,11 @@ namespace RetosMoureDev
         ///   las letras de otra palabra inicial.
         /// - NO hace falta comprobar que ambas palabras existan.
         /// - Dos palabras exactamente iguales no son anagrama.
-        ///         
+        /// </summary>
+        /// 
         /// <remarks>
         /// Dificultad: Medio
         /// </remarks>
-        /// </summary>
         public static void N2(string word1, string word2)
         {
             bool result = N2a(word1, word2);
@@ -107,11 +109,11 @@ namespace RetosMoureDev
         /// - La serie Fibonacci se compone por una sucesión de números en
         ///   la que el siguiente siempre es la suma de los dos anteriores.
         ///   0, 1, 1, 2, 3, 5, 8, 13...
-        ///   
+        /// </summary>
+        /// 
         /// <remarks>
         /// Dificultad: Dificil
         /// </remarks>
-        /// </summary>
         public static void N3()
         {
             //Usamos long en vez de int porque los numeros de la sucesión de Fibonacci pueden ser muy grandes y no cabrían en un int
@@ -143,11 +145,11 @@ namespace RetosMoureDev
         /// <summary>
         /// Escribe un programa que se encargue de comprobar si un número es o no primo.
         /// Hecho esto, imprime los números primos entre 1 y 100.
+        /// </summary>
         /// 
         /// <remarks>
         /// Dificultad: Medio
         /// </remarks>
-        /// </summary>
         public static void N4()
         {
             Console.WriteLine("Los números primos entre 1 y 100 son: ");
@@ -193,14 +195,362 @@ namespace RetosMoureDev
         /// - La función recibirá por parámetro sólo UN polígono a la vez.
         /// - Los polígonos soportados serán Triángulo, Cuadrado y Rectángulo.
         /// - Imprime el cálculo del área de un polígono de cada tipo.
+        /// </summary>
         /// 
         /// <remarks>
         /// Dificultad: Facil
         /// </remarks>
-        /// </summary>
         public static void N5(Poligono poligono)
         {
             Console.WriteLine("El área del poligono es {0}", poligono.CalcularArea());
+        }
+
+        #endregion
+
+        #region Ejercicio 6
+        /// <summary>
+        /// Crea un programa que se encargue de calcular el aspect ratio de una
+        /// imagen a partir de una url.
+        /// - Url de ejemplo:
+        ///   https://raw.githubusercontent.com/mouredevmouredev/master/mouredev_github_profile.png
+        /// - Por ratio hacemos referencia por ejemplo a los "16:9" de una
+        ///   imagen de 1920*1080px.
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// Dificultad: Difícil
+        /// </remarks>
+        public static async Task N6(string urlImagen)
+        {
+            try
+            {
+                Console.WriteLine($"Imagen a analizar alojada en {urlImagen}");
+                var ratio = await GetImageAspectRatio(urlImagen);
+                Console.WriteLine($"El aspect ratio de la imagen es: {ratio}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        private static async Task<string> GetImageAspectRatio(string urlImagen)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(urlImagen);
+                response.EnsureSuccessStatusCode();
+
+                using(var stream = await response.Content.ReadAsStreamAsync())
+                {
+                    #region Windows only
+                    var imagen = Image.FromStream(stream);
+                    int ancho = imagen.Width;
+                    int alto = imagen.Height;
+
+                    //Ahora que tenemos el ancho y alto de la imagen (1920x1080px por ejemplo) calculamos el maximo comun divisor (MCD) de ambos
+                    //y dividimos cada proporcion por este para sacar el ratio
+                    int mcd = MCD(ancho, alto);
+
+                    return $"{ancho/mcd}:{alto/mcd}";
+                    #endregion
+
+                    #region Linux/macOS
+                    // NOTE: If you're using Linux/macOS uncomment this section and comment the windows section
+                    /*
+                    using(var imagen = await SixLabors.ImageSharp.Image.LoadAsync(stream))
+                    {
+                        int ancho = imagen.Width;
+                        int alto = imagen.Height;
+                    
+                        int mcd = MCD(ancho, alto);
+                    
+                        return $"{ancho / mcd}:{alto / mcd}";
+                    }
+                    */
+                    #endregion
+                }
+            }
+        }
+
+        private static int MCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+
+            return a;
+        }
+        #endregion
+
+        #region Ejercicio 7
+        /// <summary>
+        /// Crea un programa que invierta el orden de una cadena de texto
+        /// sin usar funciones propias del lenguaje que lo hagan de forma automática.
+        /// - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// Dificultad: Fácil
+        /// </remarks>
+        public static void N7(string palabraAInvertir)
+        {
+            Console.WriteLine("La palabra original era {0}", palabraAInvertir);
+            Console.WriteLine("Y su forma invertida es {0}", InvertirPalabra(palabraAInvertir));
+        }
+
+        private static string InvertirPalabra(string palabraAInvertir)
+        {
+            char[] palabraInvertida = new char[palabraAInvertir.Length];
+
+            for(int i = 0;  i < palabraInvertida.Length; i++)
+            {
+                palabraInvertida[i] = palabraAInvertir[palabraAInvertir.Length - (i + 1)];
+            }
+
+            return new(palabraInvertida);
+        }
+        #endregion
+
+        #region Ejercicio 8
+        /// <summary>
+        /// Crea un programa que cuente cuántas veces se repite cada palabra
+        /// y que muestre el recuento final de todas ellas.
+        /// - Los signos de puntuación no forman parte de la palabra.
+        /// - Una palabra es la misma aunque aparezca en mayúsculas y minúsculas.
+        /// - No se pueden utilizar funciones propias del lenguaje que
+        ///   lo resuelvan automáticamente.
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// Dificultad: Medio
+        /// </remarks>
+        public static void N8(string oracion)
+        {
+            Console.WriteLine($"La oracion introducida es \"{oracion}\"");
+            
+            string oracionNormalizada = NormalizarOracion(oracion);
+            Console.WriteLine($"Su version normalizada es \"{oracionNormalizada}\"");
+            
+            Console.WriteLine($"Y el recuento de sus palabras por ocurrencia es:");
+            foreach(var palabraConOcurrencia in ObtenerPalabrasConOcurrencias(oracionNormalizada))
+            {
+                Console.WriteLine($"{palabraConOcurrencia.Key}: {palabraConOcurrencia.Value} veces");
+            }
+        }
+
+        private static string NormalizarOracion(string oracion) 
+        {
+            string oracionNormalizada = string.Empty;
+            string oracionEnMinusculasConTrim = oracion.ToLower().Trim();
+
+            foreach (char caracter in oracionEnMinusculasConTrim)
+            {
+                if(char.IsLetterOrDigit(caracter) || char.IsWhiteSpace(caracter))
+                {
+                    oracionNormalizada += caracter;
+                }
+            }
+
+            //Devolvemos oracion sin signos de puntuacion, espacios al principio/final y en minusculas
+            return oracionNormalizada;
+        }
+
+        private static Dictionary<string, int> ObtenerPalabrasConOcurrencias(string oracion)
+        {
+            string[] palabasDeOracion = oracion.Split(' ');
+            Dictionary<string, int> palabrasConOcurrencias = new Dictionary<string, int>();
+
+            foreach(string palabra in palabasDeOracion)
+            {
+                if(palabrasConOcurrencias.ContainsKey(palabra))
+                {
+                    palabrasConOcurrencias[palabra] += 1;
+                }
+                else
+                {
+                    palabrasConOcurrencias.Add(palabra, 1);
+                }
+            }
+
+            return palabrasConOcurrencias;
+
+            //Version más eficiente
+            /*
+             * 
+             * string[] palabasDeOracion = oracion.Split(' ');
+            Dictionary<string, int> palabrasConOcurrencias = [];
+
+            foreach(string palabra in palabasDeOracion)
+            {
+                if(!palabrasConOcurrencias.TryAdd(palabra, 1))
+                {
+                    palabrasConOcurrencias[palabra] += 1;
+                }
+            }
+
+            return palabrasConOcurrencias;
+            */
+        }
+        #endregion
+
+        #region Ejercicio 9
+        /// <summary>
+        /// Crea un programa que se encargue de transformar un número
+        /// decimal natural(decimal como numero en base 10, no un decimal de c#) a binario sin utilizar funciones propias del lenguaje que lo hagan directamente.
+        /// 
+        /// (Un numero natural es un numero entero no negativo: 0,1,2,3,4,5,...)
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// Dificultad: Fácil <br/>
+        /// <a href="https://www.cuemath.com/numbers/decimal-to-binary/">Explicacion de la conversion a Binario</a>
+        /// </remarks>
+        public static void N9(uint numero)
+        {
+            Console.WriteLine($"El numero natural {numero} en base 10 es el {ObtenerBinario(numero)} en binario");
+        }
+
+        private static string ObtenerBinario(uint numero)
+        {
+            if (numero == 0)
+                return "0";
+
+            string binario = string.Empty;
+            while(numero > 0)
+            {
+                uint resto = numero % 2;
+                binario = resto + binario;
+                numero /= 2;
+            }
+
+            return binario;
+        }
+        #endregion
+
+        #region Ejercicio 10
+        /// <summary>
+        /// Crea un programa que sea capaz de transformar texto natural a código
+        /// morse y viceversa.
+        /// - Debe detectar automáticamente de qué tipo se trata y realizar
+        ///   la conversión.
+        /// - En morse se soporta raya "—", punto ".", un espacio " " entre letras
+        ///   o símbolos y dos espacios entre palabras "  ".
+        /// - El alfabeto morse soportado será el mostrado en
+        ///   https://es.wikipedia.org/wiki/Código_morse.
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// Dificultad: Medio
+        /// </remarks>
+        public static void N10(string mensaje)
+        {
+            if(EsMorse(mensaje))
+            {
+                Console.WriteLine($"Tu mensaje \"{mensaje}\" esta en morse.");
+                Console.WriteLine($"Decodificado, el mensaje contiene:");
+                Console.WriteLine(Descodificar(mensaje));
+            }
+            else
+            {
+                Console.WriteLine($"Tu mensaje \"{mensaje}\" esta en lenguaje natural.");
+                Console.WriteLine($"Codificado, el mensaje quedaria como:");
+                Console.WriteLine(Codificar(mensaje));
+            }
+        }
+
+        private static bool EsMorse(string mensaje)
+        {
+            foreach(char letra in mensaje)
+            {
+                if (char.IsLetterOrDigit(letra)) 
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static string Codificar(string mensaje)
+        {
+            StringBuilder mensajeCodificado = new StringBuilder();
+            Dictionary<char, string> diccionarioNaturalMorse = new Dictionary<char, string>
+            {
+                {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."},
+                {'E', "."}, {'F', "..-."}, {'G', "--."}, {'H', "...."},
+                {'I', ".."}, {'J', ".---"}, {'K', "-.-"}, {'L', ".-.."},
+                {'M', "--"}, {'N', "-."}, {'O', "---"}, {'P', ".--."},
+                {'Q', "--.-"}, {'R', ".-."}, {'S', "..."}, {'T', "-"},
+                {'U', "..-"}, {'V', "...-"}, {'W', ".--"}, {'X', "-..-"},
+                {'Y', "-.--"}, {'Z', "--.."},
+                {'0', "-----"}, {'1', ".----"}, {'2', "..---"}, {'3', "...--"},
+                {'4', "....-"}, {'5', "....."}, {'6', "-...."}, {'7', "--..."},
+                {'8', "---.."}, {'9', "----."},
+                {'.', ".-.-.-"}, {',', "--..--"}, {'?', "..--.."}, {'"', ".-..-."},
+                {'/', "-..-."}, {'\\', ".—..—."}
+            };
+
+            foreach (char letra in mensaje.ToUpper())
+            {
+                if(diccionarioNaturalMorse.ContainsKey(letra))
+                {
+                    mensajeCodificado.Append(diccionarioNaturalMorse[letra]);
+                }
+                else
+                {
+                    //Util para meter espacios y puntuaciones
+                    mensajeCodificado.Append(letra);
+                }
+
+                mensajeCodificado.Append(' ');
+            }
+
+            return mensajeCodificado.ToString();
+        }
+
+        private static string Descodificar(string mensaje)
+        {
+            StringBuilder mensajeDescodificado = new StringBuilder();
+            Dictionary<string, char> diccionarioMorseNatural = new Dictionary<string, char>
+            {
+                {".-", 'A'}, {"-...", 'B'}, {"-.-.", 'C'}, {"-..", 'D'},
+                {".", 'E'}, {"..-.", 'F'}, {"--.", 'G'}, {"....", 'H'},
+                {"..", 'I'}, {".---", 'J'}, {"-.-", 'K'}, {".-..", 'L'},
+                {"--", 'M'}, {"-.", 'N'}, {"---", 'O'}, {".--.", 'P'},
+                {"--.-", 'Q'}, {".-.", 'R'}, {"...", 'S'}, {"-", 'T'},
+                {"..-", 'U'}, {"...-", 'V'}, {".--", 'W'}, {"-..-", 'X'},
+                {"-.--", 'Y'}, {"--..", 'Z'},
+                {"-----", '0'}, {".----", '1'}, {"..---", '2'}, {"...--", '3'},
+                {"....-", '4'}, {".....", '5'}, {"-....", '6'}, {"--...", '7'},
+                {"---..", '8'}, {"----.", '9'},
+                {".-.-.-", '.'}, {"--..--", ','}, {"..--..", '?'}, {".-..-.", '"'},
+                {"-..-.", '/'}, {".—..—.", '\\'}
+
+            };
+
+            foreach(string palabra in mensaje.ToUpper().Split("  "))
+            {
+                foreach(string simbolos in palabra.Split(" "))
+                {
+                    if (diccionarioMorseNatural.ContainsKey(simbolos))
+                    {
+                        mensajeDescodificado.Append(diccionarioMorseNatural[simbolos]);
+                    }
+                    else
+                    {
+                        //Util para meter espacios y puntuaciones
+                        mensajeDescodificado.Append(simbolos);
+                    }
+
+                }
+                mensajeDescodificado.Append(' ');
+            }
+
+            return mensajeDescodificado.ToString().Trim();
         }
 
         #endregion
