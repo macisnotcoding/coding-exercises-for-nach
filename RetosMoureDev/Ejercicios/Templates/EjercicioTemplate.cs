b74bebc510b238fb4c7a416e6e7d4dda1cd25da1
macisnotcoding <codingmac12@gmail.com>
2024-04-17 01:08:29 +0200

    feat: add Ejercicio template for new implementations, update docu

---
+++ RetosMoureDev/Ejercicios/Templates/EjercicioTemplate.cs
@@ -0,0 +1,17 @@
+﻿namespace RetosMoureDev.Ejercicios.Templates
+{
+    public static class EjercicioTemplate
+    {
+        public static void Run()
+        {
+            // Ejecutar la lógica del ejercicio
+            ExecuteLogic();
+        }
+
+        private static void ExecuteLogic()
+        {
+            // Lógica del ejercicio
+            Console.WriteLine("Implementar la lógica del ejercicio aquí.");
+        }
+    }
+}
